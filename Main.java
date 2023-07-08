d9feeeef03f8671d459dd46dbd7e524f52bf6ac7
Aliiah <138148845+Aliiah@users.noreply.github.com>
2023-07-08 19:17:11 +0200

    Homework_first11
    right

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
-        String room1 = ("I");
-        String room2 = ("Did");
-        String room3 = ("My");
-        String room4 = ("HomeWork");
-        System.out.println(room1 + room2 + room3 + room4);
+        String book = "hello";
+        String book2 = "hello2";
+        System.out.println(book + book2);
+        int number1 = 20;
+        System.out.println(number1);
+        int number2 = 2;
+        System.out.println(number2);
+        int number3 = number1 + number2;
+        int number4 = number1 - number2;
+        int number5 = number1 * number2;
+        int number6 = number1 / number2;
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+
 
 
     }

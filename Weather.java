cb14746c9aff570d7d75d951eeb7c3d8aa95335e
Aliiah <138148845+Aliiah@users.noreply.github.com>
2023-07-16 17:00:51 +0200

    weather
    Homework_second

---
+++ Weather.java
@@ -0,0 +1,57 @@
+public class Weather {
+    public String nameOfTheDay1 = "Monday";
+    public String descriptionOfTheDay1 = "Windy";
+    public int weatherOfTheDay1 = 22;
+
+    public String getNameOfTheDay2 = "Tuesday";
+    public String getDescriptionOfTheDay2 = "Rainy";
+    public int getWeatherOfTheDay2 = 21;
+
+    public String nameOfTheDay3 = "Wednesday";
+    public String getDescriptionOfTheDay3 = "Stormy";
+    public double getWeatherOfTheDay3 = 19.5;
+
+    public String nameOfTheDay4 = "Thursday";
+    public String getDescriptionOfTheDay4 = "Foggy";
+    public double getWeatherOfTheDay4 = 21.7;
+
+    public String nameOfTheDay5 = "Friday";
+    public String getDescriptionOfTheDay5 = "Sunny";
+    public int getWeatherOfTheDay5 = 23;
+
+    public String nameOfTheDay6 = "Saturday";
+    public String getDescriptionOfTheDay6 = "Dry";
+    public double getWeatherOfTheDay6 = 25.7;
+
+    public String nameOfTheDay7 = "Sunday";
+    public String getDescriptionOfTheDay7 = "Still";
+    public double getWeatherOfTheDay7 = 28.5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
